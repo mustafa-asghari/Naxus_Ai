129b7b1f68580e72d4a6b42b03e2c4082fb762d6
Mustafa Asghari <asghari.mustafa99@gmail.com>
2026-01-07 13:53:31 +1000

    Remove ignored cache files and logs from git tracking

--- run_nexus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-cd /Users/mustafaasghari/code/Nexus
-export PATH="/Users/mustafaasghari/code/Nexus/vnev/bin:$PATH"
-exec /Users/mustafaasghari/code/Nexus/vnev/bin/python /Users/mustafaasghari/code/Nexus/nexus.py
